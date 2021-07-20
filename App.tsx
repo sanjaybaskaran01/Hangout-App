45c81077975bbcd5887e6a932fe47a77901af95e
Sanjay Baskaran <sanjaybaskaran01@gmail.com>
2021-07-20 18:16:40 +0530

    fix: top nav obstructing notch

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import "react-native-gesture-handler";
 import React from "react";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Signin from "./screens/Signin";
--- screens/DrawerNavigationRoutes.tsx
+++ screens/DrawerNavigationRoutes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet,StatusBar } from "react-native";
 import Friends from "./TabScreens/Friends";
 import Plans from "./TabScreens/Plans";
 
@@ -10,21 +10,30 @@ const Drawer = createDrawerNavigator();
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
       <Tab.Navigator>
         <Tab.Screen name="Plans" component={Plans} />
         <Tab.Screen name="Friends" component={Friends} />
       </Tab.Navigator>
-    </SafeAreaView>
+
   );
 };
 
 const DrawerNavigationRoutes = () => {
   return (
+    <SafeAreaView style={styles.SafeArea}>
       <Drawer.Navigator>
         <Drawer.Screen name="Home" component={HomeScreen} />
       </Drawer.Navigator>
+      </SafeAreaView>
   );
 };
 
 export default DrawerNavigationRoutes;
+
+const styles=StyleSheet.create({
+    SafeArea: {
+      flex: 1,
+    //   backgroundColor: "white",
+      paddingTop: StatusBar.currentHeight
+    }
+  });
